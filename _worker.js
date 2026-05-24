0fb136468229151dcff82d2560edfa1d8b1c3572
qianxiu203 <2037710878@qq.com>
2026-05-24 22:24:34 +0800

    替换非WebSocket处理为静态nginx伪装页

--- _worker.js
+++ _worker.js
@@ -79,12 +79,8 @@ export default {
       const url = new URL(r.url);
       
       if (r.headers.get('Upgrade') !== 'websocket') {
-          const t = new URL(r.url);
-          t.hostname = 'www.w3.org';
-          t.protocol = 'https:';
-          const h = new Headers(r.headers);
-          h.set('Host', t.hostname);
-          return fetch(new Request(t.toString(), { method: r.method, headers: h, body: r.body, redirect: 'follow' }));
+          const html = '<!DOCTYPE html>\n<html>\n<head>\n<title>Welcome to nginx!</title>\n<style>\nbody { width: 35em; margin: 0 auto; font-family: Tahoma, Verdana, Arial, sans-serif; }\n</style>\n</head>\n<body>\n<h1>Welcome to nginx!</h1>\n<p>If you see this page, the nginx web server is successfully installed and working. Further configuration is required.</p>\n<p>For online documentation and support please refer to <a href="http://nginx.org/">nginx.org</a>.<br/>\nCommercial support is available at <a href="http://nginx.com/">nginx.com</a>.</p>\n<p><em>Thank you for using nginx.</em></p>\n</body>\n</html>';
+          return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
       }
 
       const _UUID = env.KEY ? await getDynamicUUID(env.KEY) : getEnv(env, 'UUID', UUID);
