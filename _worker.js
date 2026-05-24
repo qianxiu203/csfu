e0422ad0294e1aa61844c757842bbe6688b5c462
qianxiu203 <2037710878@qq.com>
2026-05-24 22:30:03 +0800

    非WebSocket拦截增加大小写容错处理

--- _worker.js
+++ _worker.js
@@ -78,7 +78,8 @@ export default {
     try {
       const url = new URL(r.url);
       
-      if (r.headers.get('Upgrade') !== 'websocket') {
+      const upgradeHeader = r.headers.get('Upgrade');
+      if (!upgradeHeader || upgradeHeader.toLowerCase() !== 'websocket') {
           const html = '<!DOCTYPE html>\n<html>\n<head>\n<title>Welcome to nginx!</title>\n<style>\nbody { width: 35em; margin: 0 auto; font-family: Tahoma, Verdana, Arial, sans-serif; }\n</style>\n</head>\n<body>\n<h1>Welcome to nginx!</h1>\n<p>If you see this page, the nginx web server is successfully installed and working. Further configuration is required.</p>\n<p>For online documentation and support please refer to <a href="http://nginx.org/">nginx.org</a>.<br/>\nCommercial support is available at <a href="http://nginx.com/">nginx.com</a>.</p>\n<p><em>Thank you for using nginx.</em></p>\n</body>\n</html>';
           return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
       }
